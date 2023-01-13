53ba6540ebb990945103b16cc0603c1cc6df8759
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-01-13 16:39:13 +0300

    Pt 8

--- src/router/routes.js
+++ src/router/routes.js
@@ -18,6 +18,18 @@ const routes = [
             path: 'saved-routes', component: () => import('pages/route/SavedRoutes.vue')
           }
         ]
+      },
+      {
+        path: '/auth',
+        component: () => import('pages/AuthPage.vue'),
+        children: [
+          {
+            path: '', component: () => import('pages/auth/LoginSection.vue')
+          },
+          {
+            path: 'register', component: () => import('pages/auth/RegisterSection.vue')
+          }
+        ]
       }
     ]
   },
